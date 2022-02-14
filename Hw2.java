c5f123f25f5d909cc0f43e4adfe27ae59f3481c0
Paulette YIGO <paulette@Paulettes-MacBook-Air.local>
2022-02-14 16:38:48 -0500

    initial commit

---
+++ Hw2.java
@@ -0,0 +1,30 @@
+public class Hw2 {
+//    Write a Java program to copy one array list into another
+//
+//    Write a Java program to extract a portion of an array list
+//
+//    Write a Java program of swap two elements in an array list
+//
+//    Write a Java program to test an array list is empty or not
+//
+//    Write a Java program to replace the second element of an ArrayList with the specified element.
+//
+//    Write a Java program to trim the capacity of an array list the current list size
+//
+//    HUNGRY FOR MORE
+//
+//    Write a Java program to test a hash set is empty or not.
+//
+//    Write a Java program to get the number of elements in a hash set
+//
+//    Write a Java program to iterate through all elements in a hash list.
+//
+//    Write a Java program to convert a hash set to an array.
+//
+//    Write a Java program to compare two sets and retain elements that are the same on both sets.
+
+
+    public static void main(String[] args) {
+
+    }
+}
