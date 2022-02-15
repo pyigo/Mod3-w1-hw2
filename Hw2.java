2f7f77e98a328be3a548cd880eed6498aa38e55e
paulette <paulette.yigo@gmail.com>
2022-02-14 23:32:06 -0500

    hw complete

--- Hw2.java
+++ Hw2.java
@@ -1,24 +1,13 @@
-
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public class Hw2 {
 
-//    Write a Java program to extract a portion of an array list
-//
-//    Write a Java program of swap two elements in an array list
-//
-//    Write a Java program to test an array list is empty or not
-//
-//    Write a Java program to replace the second element of an ArrayList with the specified element.
-//
-//    Write a Java program to trim the capacity of an array list the current list size
-
-
     public static void main(String[] args) {
-        ArrayList<String> myVowels = new ArrayList<String>();
-//        adding vowels to my list of vowels
+        //*************Write a Java program to copy one array list into another*************
+        ArrayList<String> myVowels = new ArrayList<>();
+        //adding vowels to my list of vowels
         myVowels.add("a");
         myVowels.add("e");
         myVowels.add("i");
@@ -26,8 +15,8 @@ public class Hw2 {
         myVowels.add("u");
         System.out.println(" List of vowels:" + myVowels);
 
-//        creating consonant list and adding elements
-        ArrayList<String> myConsonants = new ArrayList<String>();
+        //creating consonant list and adding elements
+        ArrayList<String> myConsonants = new ArrayList<>();
         myConsonants.add("b");
         myConsonants.add("c");
         myConsonants.add("d");
@@ -35,13 +24,13 @@ public class Hw2 {
         myConsonants.add("g");
         System.out.println("List of consonants :" + myConsonants);
 
-//        using Collections.copy to copy list of myVowels inside list of MyConsonants
+        //using Collections.copy to copy list of myVowels inside list of MyConsonants
         Collections.copy(myConsonants, myVowels);
         System.out.println("List of copied elements :" + myConsonants);
 
 
-//        Write a Java program to extract a portion of an array list
-//        Creating and adding elements to original list
+        //*************Write a Java program to extract a portion of an array list*************
+        //Creating and adding elements to original list
         ArrayList<String> groceriesList = new ArrayList<>();
         groceriesList.add("veggies");
         groceriesList.add("dairy");
@@ -50,11 +39,42 @@ public class Hw2 {
         groceriesList.add("beauty");
         System.out.println("Original groceries list:" + groceriesList);
 
-//        using subList() method to extract last 2 items of groceriesList
+        //using subList() method to extract last 2 items of groceriesList
        List<String> shorterList = groceriesList.subList (3,4);
         System.out.println("shorter groceries list:" + shorterList );
 
+        //*************Write a Java program of swap two elements in an array list*************
+        //// Create and populating the Array List
+        ArrayList<String> count = new ArrayList<>(10);
+        count.add("one");
+        count.add("two");
+        count.add("three");
+        count.add("four");
+        count.add("five");
+        //display original count list
+        System.out.println("Count list before swap:" + count);
 
+        // Swapping the elements at 0 and 1 indices using Collections.swap() and display new list after swap
+        Collections.swap(count, 0, 1);
+        System.out.println("Count list after swap:" + count);
 
-    }
+        //*************Write a Java program to test an array list is empty or not*************
+        //using isEmpty()  to check if count list is empty
+        boolean response= count.isEmpty();
+        if (response==true){
+            System.out.println("Count list is empty");
+        }else{
+            System.out.println("Count list is not empty");
+        }
+
+        //*************Write a Java program to replace the second element of an ArrayList with the specified element.*************
+        //using set() method to replace beauty element by baby food in groceries list
+        groceriesList.set(4,"baby food");
+        System.out.println("New groceries list updated :" + groceriesList);
+
+        //*************Write a Java program to trim the capacity of an array list the current list size*************
+        //using trimToSize() to trim myConsonants list size to specified number of elements
+        count.trimToSize();
+        System.out.println("Trimed to size count list:"+ count);
+}
 }
